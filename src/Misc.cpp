e04e0a93cd12b0247950e0746372acc5d6f2862e
Luckshya <luckshyaverma@gmail.com>
2020-03-19 16:36:13 +0530

    Added empty checks before mutex locks

--- src/Misc.cpp
+++ src/Misc.cpp
@@ -19,28 +19,33 @@ namespace SqHTTP {
 
 	void refreshFutureHolder() {
 
-		std::ostringstream msg;
-		msg << "[FUTURE HOLDER] Before clearing: " << future_holder.size();
-		OutputDebug(msg.str().c_str());
-
+		if (!future_holder.empty())
 		{
-			std::lock_guard<std::mutex> lock(m_futureGuard);
-			future_holder.erase(
-				std::remove_if(future_holder.begin(), future_holder.end(), [](std::future<bool> & fr) {
-				return fr.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
-			}),
-				future_holder.end()
-				);
-		}
+			std::ostringstream msg;
+			msg << "[FUTURE HOLDER] Before clearing: " << future_holder.size();
+			OutputDebug(msg.str().c_str());
 
-		std::ostringstream msg2;
-		msg2 << "[FUTURE HOLDER] After clearing: " << future_holder.size();
-		OutputDebug(msg2.str().c_str());
+			{
+				std::lock_guard<std::mutex> lock(m_futureGuard);
+				future_holder.erase(
+					std::remove_if(future_holder.begin(), future_holder.end(), [](std::future<bool> & fr) {
+					return fr.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+				}),
+					future_holder.end()
+					);
+			}
+
+			std::ostringstream msg2;
+			msg2 << "[FUTURE HOLDER] After clearing: " << future_holder.size();
+			OutputDebug(msg2.str().c_str());
+		}
 	}
 
 	void clearFuture() {
-		std::lock_guard<std::mutex> lock(m_futureGuard);
-		future_holder.clear();
+		if (!future_holder.empty()) {
+			std::lock_guard<std::mutex> lock(m_futureGuard);
+			future_holder.clear();
+		}
 	}
 
 } // Namespace - SqHTTP
